ffaea4fc4e70e08d6be78c4e0ab4ffbcd091e69a
Fabian Murer <fabian@fabianmurer.ch>
2019-01-15 13:18:30 +0100

    use variables

--- Collect.bat
+++ Collect.bat
@@ -15,13 +15,14 @@ REM  ################################################
 SET elevate=.\include\elevate.cmd
 SET audit=.\include\a.bat
 SET sys=.\include\s.bat
+SET outdir=.\output\
 SET args=%1
 
 IF NOT EXIST "%elevate%" goto :failed
 IF NOT EXIST "%audit%" goto :failed
 IF NOT EXIST "%sys%" goto :failed
 
-IF NOT EXIST "%output%" mkdir .\output
+IF NOT EXIST "%outdir%" mkdir .\output
 
 For /f "tokens=2 delims=[]" %%G in ('ver') Do (set _version=%%G) 
 For /f "tokens=2,3,4 delims=. " %%G in ('echo %_version%') Do (set _major=%%G& set _minor=%%H& set _build=%%I) 
